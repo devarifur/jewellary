ea888134a4d4416c210fe5b967aa3c0a6d23f938
Arifur Rahman <arifsarder19821@gmail.com>
2025-11-07 22:14:36 +0600

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a953cbd382a7fe765918ec3a4ab8c2f96b20eb46',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a953cbd382a7fe765918ec3a4ab8c2f96b20eb46',
+    ),
+  ),
+);
